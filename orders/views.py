6a51466361f1b3420bdf25b71ba630d799dd19d6
mark musasizi <markmusasizi@gmail.com>
2014-10-03 18:07:37 +0200

    wire upw our smartmin backend

--- orders/views.py
+++ orders/views.py
@@ -1,1 +1,17 @@
 # Create your views here.
+#Views used by order administrators
+
+from smartmin.views import *
+from .models  import *
+
+class OrderListCRUDL(SmartCRUDL):
+  model = OrderList
+  actions = ("list","read",'update')
+  permissions = True
+
+  #override list objec
+  class List(SmartListView):
+    fields = ('scheduled_time','title','user','order.mobile','order.address','order.comment')
+
+  class Read(SmartReadView):
+    fields = ('title','user','order.address','order.mobile','order.address','order.comment')
