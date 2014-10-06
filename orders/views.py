08bf956bccb3b9679e7e548b367588d523a1cd88
mark musasizi <markmusasizi@gmail.com>
2014-10-06 08:35:14 +0200

    get a list of items

--- orders/views.py
+++ orders/views.py
@@ -11,7 +11,16 @@ class OrderListCRUDL(SmartCRUDL):
 
   #override list objec
   class List(SmartListView):
+#    import pdb;pdb.set_trace()
     fields = ('scheduled_time','title','user','order.mobile','order.address','order.comment')
 
   class Read(SmartReadView):
-    fields = ('title','user','order.address','order.mobile','order.address','order.comment')
+    fields = ('title','user','order.address','listItems','order.mobile','order.address','order.comment')
+
+    def get_listItems(self,obj):
+      #make sure to return items that belong only to this user
+      collection = obj.item_set.all()
+      items_in_list=[]
+      for i in collection:
+        items_in_list.append(i.item_description)
+      return items_in_list
