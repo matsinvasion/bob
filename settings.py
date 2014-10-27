50ebb5ca6ec4a0f46d24455dab9fffd1c1d1d2dc
mark musasizi <markmusasizi@gmail.com>
2014-10-27 13:16:10 +0200

    set debug to false in production

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 # Django settings for freshbunch project.
 import os
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -26,7 +26,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["www.markshelp.herokuapp.com","markshelp.herokuapp.com"]
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
