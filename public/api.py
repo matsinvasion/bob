d3bc4bdc2a5896ddcf637cb9700493cb5d63661e
mark musasizi <markmusasizi@gmail.com>
2014-10-29 16:23:27 +0200

    configure remote login and logout functionality

--- public/api.py
+++ public/api.py
@@ -4,14 +4,12 @@ from orders.models import OrderList,Order,Assignment
 from tastypie import fields
 from tastypie.authorization import Authorization, DjangoAuthorization
 from django.contrib.auth.models import User
-from django.core import urlresolvers
 from django.http import HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from tastypie.http import HttpUnauthorized, HttpForbidden
+from django.conf.urls import url
+from tastypie.utils import trailing_slash
 import datetime
-from tastypie.exceptions import BadRequest
-from django.db import IntegrityError
-from tastypie_extras.exceptions import CustomBadRequest
-from django.contrib.auth.hashers import make_password
-from tastypie.authentication import ApiKeyAuthentication
 from django.db import models
 from registration.models import RegistrationProfile
 from registration import signals
@@ -35,6 +33,52 @@ class UserResource(ModelResource):
               'id':ALL,
     }
     cache = SimpleCache(timeout=10);
+
+  #provide login urls
+
+  def override_urls(self):
+    return [
+    url(r"^(?P<resource_name>%s)/login%s$" %(self._meta.resource_name, trailing_slash()),self.wrap_view('login'), name="api_login"),
+    url(r'^(?P<resource_name>%s)/logout%s$' %(self._meta.resource_name, trailing_slash()),self.wrap_view('logout'), name='api_logout'),
+    ]
+
+  #provide login functionality
+  def login(self,request,**kwargs):
+    self.method_check(request, allowed=['post'])
+    data = self.deserialize(request, request.body, format=request.META.get('CONTENT_TYPE', 'application/json'))
+    username = data.get('username', '')
+    password = data.get('password', '')
+
+    user = authenticate(username=username,password=password)
+    if user:
+      if user.is_active:
+        login(request,user)
+        return self.create_response(request, {'success': True})
+      else:
+        return self.create_response(request, {
+                    'success': False,
+                    'reason': 'disabled',
+                    }, HttpForbidden )
+    else:
+      return self.create_response(request, {
+                'success': False,
+                'reason': 'incorrect',
+                }, HttpUnauthorized )
+
+  def logout(self, request, **kwargs):
+        self.method_check(request, allowed=['get'])
+        if request.user and request.user.is_authenticated():
+            logout(request)
+            return self.create_response(request, { 'success': True })
+        else:
+            return self.create_response(request, { 'success': False }, HttpUnauthorized)
+
+
+
+
+
+
+
 
 
   #django-registration does the registering | consistency
